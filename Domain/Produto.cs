6b7042ea622180f3868bb930a81495272f71057c
Carlos Silva <carlosferranti@gmail.com>
2022-09-02 21:07:30 -0300

    Incrementações no código.

---
+++ Domain/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Produto
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(100, ErrorMessage = "O campo deve ter no máximo 100 caracteres")]
+        public string Nome { get; set; }
+
+        public int  Quantidade { get; set; }
+
+        public int Status { get; set; }
+        
+    }
+}
