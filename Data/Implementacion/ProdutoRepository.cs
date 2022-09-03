730930e35936cc3d05d821a181ba8fe1407cf9c8
Carlos Silva <carlosferranti@gmail.com>
2022-09-02 21:07:30 -0300

    Add project files.

---
+++ Data/Implementacion/ProdutoRepository.cs
@@ -0,0 +1,38 @@
+﻿using Data.Interfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Implementacion
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly Contexto _db;
+
+        public ProdutoRepository(Contexto db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(ProdutoRepository entity)
+        {
+            await _db.AddAsync(entity);
+            await _db.SaveChangesAsync();
+        }
+
+        public Task Add(Produto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Produto> GetAll()
+        {
+
+            return _db.Produto;
+
+        }
+    }
+}
---
+++ Data/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IProdutoRepository
+    {
+        IQueryable<Produto> GetAll();
+        Task Add(Produto entity);
+    }
+}
